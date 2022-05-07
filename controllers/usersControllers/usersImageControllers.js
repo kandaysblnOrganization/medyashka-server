82ccbb503a15561e30b5680d4d7e1b0d7cd986d6
stambekovbera <87119093+stambekovbera@users.noreply.github.com>
2022-05-08 01:53:12 +0500

    fix image upload

--- controllers/usersControllers/usersImageControllers.js
+++ controllers/usersControllers/usersImageControllers.js
@@ -1,5 +1,27 @@
+const uuid = require('uuid');
+const path = require('path');
+const ApiError = require('../../errors/ApiErrors');
+const {
+    usersImage
+} = require('../../models/models');
+
 class UsersImageControllers {
     putUserImage = async (req, res, next) => {
+        const {userId} = req.params;
+        const {avatar} = req.files;
+        const fileName = uuid.v4() + '.jpg';
+        avatar.mv(path.resolve(__dirname, '../../static/avatars', fileName));
+
+        const userImage = await usersImage.findOne({where: {userId}})
+            .then(res => {
+                if (!res) {
+                    return ApiError.badRequest('Данные об изображении не найдены');
+                }
+
+                return res.update({avatar: fileName})
+            })
+
+        return res.json(userImage);
     };
     getUserImage = async (req, res, next) => {
     };
