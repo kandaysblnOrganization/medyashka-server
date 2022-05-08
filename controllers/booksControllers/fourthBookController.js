552f562dc00fa9d5aa4b16ce99eb20b0dbcad35f
stambekovbera <87119093+stambekovbera@users.noreply.github.com>
2022-05-08 13:38:41 +0500

    fix

--- controllers/booksControllers/fourthBookController.js
+++ controllers/booksControllers/fourthBookController.js
@@ -1,5 +1,33 @@
+const {fourthBook} = require("../../models/models");
+const ApiError = require("../../errors/ApiErrors");
+
 class FourthBookController {
-    getBook = async () => {
+    getBook = async (req, res, next) => {
+        try {
+            let {page} = req.query;
+            page = +page || 1;
+            const limit = 1;
+            const offset = page * limit - limit;
+            const order = [
+                ["id", "ASC"]
+            ];
+
+            let book = await fourthBook.findAndCountAll({limit, offset, order})
+                .then(res => {
+                    if (!res) {
+                        return ApiError.badRequest("Страница не существует");
+                    }
+                    if (res.rows.length === 0) {
+                        return ApiError.badRequest("Страница не существует");
+                    }
+
+                    return res;
+                })
+
+            return res.json(book);
+        } catch (err) {
+            return next(ApiError.badRequest(err.message));
+        }
     };
 }
 
